661f2970995280796d6ee391095e30836cf36a29
Lianneke <aendenroomer.l@gmail.com>
2021-03-30 11:24:41 +0200

    No extra

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -18,6 +21,32 @@ public class Main {
         WeatherForecast weatherForecast2 = new WeatherForecast(6);
         WeatherForecast weatherForecast3 = new WeatherForecast(8);
         WeatherForecast weatherForecast4 = new WeatherForecast(11);
+
+        Piste piste1 = new Piste("après-ski", "zwart", 2950);
+        Piste piste2 = new Piste("Le chasse-neige", "rood", 2950);
+        Piste piste3 = new Piste("La Station", "rood", 2050);
+        Piste piste4 = new Piste("Le tire-fesses", "blauw", 2050);
+        Piste piste5 = new Piste("Les bosses", "rood", 2050);
+        Piste piste6 = new Piste("La dameuse", "blauw", 1100);
+        Piste piste7 = new Piste("Le débutant", "groen", 1100);
+        Piste piste8 = new Piste("La piste verte", "blauw", 1100);
+        Piste piste9 = new Piste("La bleue", "groen", 1100);
+
+        List<Piste> pistes2950m = new ArrayList<>();
+        pistes2950m.add(piste1);
+        pistes2950m.add(piste2);
+
+        List<Piste> pistes2050m = new ArrayList<>();
+        pistes2050m.add(piste3);
+        pistes2050m.add(piste4);
+        pistes2050m.add(piste5);
+
+        List<Piste> pistes1100m = new ArrayList<>();
+        pistes1100m.add(piste6);
+        pistes1100m.add(piste7);
+        pistes1100m.add(piste8);
+        pistes1100m.add(piste9);
+
 
 
         gondola1.controlAccess(tourist1);
---
+++ src/Piste.java
@@ -0,0 +1,24 @@
+public class Piste {
+
+    private String name;
+    private String color;
+    private int hight;
+
+    public Piste(String name, String color, int hight) {
+        this.name = name;
+        this.color = color;
+        this.hight = hight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getHight() {
+        return hight;
+    }
+}
